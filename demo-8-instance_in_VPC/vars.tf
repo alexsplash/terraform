afc2942ac60fc9f4f772c512ca3650a89873a720
alexsplash <oleksii.yanushkevych@gmail.com>
2019-05-02 17:44:38 +0300

    EC2 in VPC

---
+++ demo-8-instance_in_VPC/vars.tf
@@ -0,0 +1,21 @@
+variable "AWS_REGION" {
+  default = "eu-west-1"
+}
+
+variable "PATH_TO_PRIVATE_KEY" {
+  default = "lesha_key"
+}
+
+variable "PATH_TO_PUBLIC_KEY" {
+  default = "lesha_key.pub"
+}
+
+variable "AMIS" {
+  type = "map"
+
+  default = {
+    us-east-1 = "ami-13be557e"
+    us-west-2 = "ami-06b94666"
+    eu-west-1 = "ami-08d658f84a6d84a80"
+  }
+}
