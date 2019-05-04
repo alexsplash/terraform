0c963b3914ef31068e3b6eea8fecf722040ed35b
alexsplash <oleksii.yanushkevych@gmail.com>
2019-05-04 16:31:07 +0300

    Route53

--- demo-10-Userdata_(install docker and lvm)/instance.tf
+++ demo-10-Userdata_(install docker and lvm)/instance.tf
@@ -9,25 +9,24 @@ resource "aws_instance" "example" {
   vpc_security_group_ids = ["${aws_security_group.allow-ssh.id}"]
 
   # the public SSH key
-  key_name = "${aws_key_pair.mykeypair.key_name}"
+  key_name = "${aws_key_pair.lesha_key.key_name}"
 
   # user data
   user_data = "${data.template_cloudinit_config.cloudinit-example.rendered}"
-
 }
 
 resource "aws_ebs_volume" "ebs-volume-1" {
-    availability_zone = "eu-west-1a"
-    size = 20
-    type = "gp2" 
-    tags {
-        Name = "extra volume data"
-    }
+  availability_zone = "eu-west-1a"
+  size              = 20
+  type              = "gp2"
+
+  tags {
+    Name = "extra volume data"
+  }
 }
 
 resource "aws_volume_attachment" "ebs-volume-1-attachment" {
   device_name = "${var.INSTANCE_DEVICE_NAME}"
-  volume_id = "${aws_ebs_volume.ebs-volume-1.id}"
+  volume_id   = "${aws_ebs_volume.ebs-volume-1.id}"
   instance_id = "${aws_instance.example.id}"
 }
-
