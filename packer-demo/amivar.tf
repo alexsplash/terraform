d74bc2a3674c5741735e3f5d5b9303c9f792c58e
lesha <lesha@leshas-MacBook-Air.local>
2019-06-29 09:56:29 +0300

    paker demo

---
+++ packer-demo/amivar.tf
@@ -0,0 +1,1 @@
+variable "AMI_ID" { default = "ami-078dbd63abbd84983" }
--- packer-demo/vars.tf
+++ packer-demo/vars.tf
@@ -3,9 +3,9 @@ variable "AWS_REGION" {
 }
 
 variable "PATH_TO_PRIVATE_KEY" {
-  default = "mykey"
+  default = "lesha_key"
 }
 
 variable "PATH_TO_PUBLIC_KEY" {
-  default = "mykey.pub"
+  default = "lesha_key.pub"
 }
