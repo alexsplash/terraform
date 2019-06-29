c6b6963d928e79f65b8691be5445273ec2696a97
alexsplash <oleksii.yanushkevych@gmail.com>
2019-06-29 09:17:36 +0300

    renaming

--- docker-demo-2/key.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_key_pair" "lesha_key" {
-  key_name   = "lesha_key"
-  public_key = "${file("${var.PATH_TO_PUBLIC_KEY}")}"
-
-  lifecycle {
-    ignore_changes = ["public_key"]
-  }
-}
